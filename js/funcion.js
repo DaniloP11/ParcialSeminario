eb57e8997f41e1d2daad20b5c37b15b3a12e4452
DaniloP11 <108538589+DaniloP11@users.noreply.github.com>
2022-10-07 10:32:51 -0500

    Danilo_reto3

---
+++ js/funcion.js
@@ -0,0 +1,11 @@
+
+function btn2(){
+        var sala=document.getElementById("boton2").value;
+        var cant=document.getElementById("txt4").value;
+    
+        num1 = prompt("Digita Valor:");
+
+        document.getElementById("textprest").innerHTML=cant*num1;
+}
+
+
